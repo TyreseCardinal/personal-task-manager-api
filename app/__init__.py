dc0618d3db57c192a040958b867a228cbd82d510
Tyrese <tyrese.k.cardinal@shaw.ca>
2024-09-07 07:20:19 -0600

    Installed and implemented CORS
    - Installed Flask CORS and enabled it globally within the '__init__.py' file
    
    API remains functional :)

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from app.config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app)
 
     db.init_app(app)
     
